b5893ded3ff9dfefb79e0820f86ff478efff019b
Vladimir Tesic <vladimir.tesic@serengetitech.com>
2021-04-26 08:19:09 +0200

    Add courses list and side navigation. 	new file:   proxy.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/courses-card-list/courses-card-list.component.html 	new file:   src/app/courses-card-list/courses-card-list.component.scss 	new file:   src/app/courses-card-list/courses-card-list.component.spec.ts 	new file:   src/app/courses-card-list/courses-card-list.component.ts 	modified:   src/app/home/home.component.html 	modified:   src/app/home/home.component.ts 	modified:   src/app/model/course.ts 	new file:   src/app/top-menu/top-menu.component.html 	new file:   src/app/top-menu/top-menu.component.scss 	new file:   src/app/top-menu/top-menu.component.spec.ts 	new file:   src/app/top-menu/top-menu.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {COURSES} from "../model/db-data";
+import {Course} from '../model/course';
+import {COURSES} from '../model/db-data';
 
 @Component({
     selector: 'home',
@@ -8,9 +8,7 @@ import {COURSES} from "../model/db-data";
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
     beginnerCourses: Course[];
-
     advancedCourses: Course[];
 
     constructor() {
@@ -18,11 +16,8 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
-
-        const courses:any = Object.values(COURSES);
-
+        const courses: Course[] = Object.values(COURSES);
         this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
-
         this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
     }
 
